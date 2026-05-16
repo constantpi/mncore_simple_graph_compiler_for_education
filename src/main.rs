d6a2dfdffb21c70973a8a8a8d99e9020c0663c51
constantpi <wada.zkai@gmail.com>
2026-05-16 20:17:23 +0900

    cppコードの生成

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod gen_cpp;
+mod operators;
+mod utils;
+
+use color_eyre::eyre::Result;
+use tract_onnx::prelude::*;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let model_path = "/tmp/train_step/model.onnx";
+    let Ok(model_proto) = tract_onnx::onnx().proto_model_for_path(model_path) else {
+        return Err(color_eyre::eyre::eyre!("Failed to load model proto"));
+    };
+    let Ok(model) = tract_onnx::onnx().model_for_proto_model(&model_proto) else {
+        return Err(color_eyre::eyre::eyre!("Failed to load model from proto"));
+    };
+    let cpp_code = gen_cpp::generate_cpp_code(model_proto)?;
+    println!("{}", cpp_code);
+    Ok(())
+}
